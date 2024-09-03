9dea1061aadbe51f1199759853a3b206467826e1
Darie-Nistor Nicolae <darie.nistornicolae@coolblue.nl>
2024-09-03 15:01:32 +0200

    Initial Commit

---
+++ BluetoothProject/BluetoothProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothProjectApp.swift
+//  BluetoothProject
+//
+//  Created by Darie-Nistor Nicolae on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BluetoothProject
+//
+//  Created by Darie-Nistor Nicolae on 03.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
